f110788e0b288539c1c8ea5d94f3f393f5da5832
ManarElkady123 <manaralkady832@gmail.com>
2025-07-30 22:22:09 +0300

    first commit

--- lib/cubits/auth_state.dart
+++ lib/cubits/auth_state.dart
@@ -1,49 +1,4 @@
 
-// part of 'auth_cubit.dart';
-
-// abstract class AuthState extends Equatable {
-//   const AuthState();
-  
-//   @override
-//   List<Object> get props => [];
-// }
-
-// class AuthInitial extends AuthState {}
-// class AuthLoading extends AuthState {}
-// class AuthLoggedOut extends AuthState {}
-
-// class AuthSuccess extends AuthState {
-//   final UserModel user;
-//   const AuthSuccess(this.user);
-  
-//   @override
-//   List<Object> get props => [user];
-// }
-
-// class AuthRegistered extends AuthState {
-//   final UserModel user;
-//   const AuthRegistered(this.user);
-  
-//   @override
-//   List<Object> get props => [user];
-// }
-
-// class AuthResetEmailSent extends AuthState {
-//   final String email;
-//   const AuthResetEmailSent(this.email);
-  
-//   @override
-//   List<Object> get props => [email];
-// }
-
-// class AuthError extends AuthState {
-//   final String message;
-//   final String field; // To identify which field has error
-//   const AuthError(this.message, this.field);
-  
-//   @override
-//   List<Object> get props => [message, field];
-// }
 
 part of 'auth_cubit.dart';
 
